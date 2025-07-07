1e763c117263525a6bf24f0fa0b85c17a50232cf
til17005 <matthew_tilton@outlook.com>
2025-07-07 13:54:34 -0600

    comments

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,17 +1,23 @@
+/**** Create Cards ****/
+
+// Fetch and display member data from my local JSON file
 const url = 'data/members.json';
+// Select the cards container where member data will be displayed
 const cards = document.querySelector('#cards');
 
+// Get (fetch) member data from my JSON file and display it using the displayMembers function below
 const getMemberData = async () => {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        // console.table (data);
+        // console.table (data); // Commented out for debugging purposes
         displayMembers(data.companies);
     } catch (error) {
         console.error('Error fetching member data:', error);
     }
 }
 
+// My function to create and display member cards
 const displayMembers = (members) => {
     members.forEach(member => {
         const card = document.createElement('section');
@@ -49,21 +55,28 @@ const displayMembers = (members) => {
     });
 }
 
+// Call getMemberData. This will fetch the data from the JSON file and display it on the page using the displayMembers function.
 getMemberData();
 
 
 /**** Adjust View ****/
+// This section of code allows switching between the grid and list views.
+
+// Select the grid and list buttons, and the display area for member cards
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 const display = document.querySelector("#cards");
 
+// This adds an event listener to the grid button. When clicked, it will switch the display to grid view.
 gridButton.addEventListener("click", () => {
     display.classList.add("grid");
     display.classList.remove("list");
 });
 
+// This adds an event listener to the list button. When clicked, it will switch the display to list view.
 listButton.addEventListener("click", showList);
 
+// This function switches the display to list view by adding the "list" class and removing the "grid" class.
 function showList() {
     display.classList.add("list");
     display.classList.remove("grid");
