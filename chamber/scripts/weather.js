8be859ec26904a7c6f35a8b83d732af5a790d8a5
til17005 <matthew_tilton@outlook.com>
2025-07-09 21:43:15 -0600

    weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,8 +1,6 @@
 // Select the current and forecast temperature containers
 const currentTemp = document.querySelector('.current');
-
-const day = document.querySelector('.day');
-const temperature = document.querySelector('.temperature');
+const forecastTemp = document.querySelector(".forecast");
 
 // My API key
 const myAPI = '6f3bf18df5dd39d2e4dbbdffb43ff880';
@@ -40,7 +38,7 @@ async function getForecastWeather() {
         const response = await fetch(forecastWeatherURL);
         if (response.ok) {
             const forecastData = await response.json();
-            console.log(forecastData); // For my testing only
+            //console.log(forecastData); // For my testing only
             displayForecastWeather(forecastData);
         } else {
             throw Error(await response.text());
@@ -75,10 +73,10 @@ function displayCurrentWeather(weatherData) {
     icon.setAttribute('src', `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`);
     icon.setAttribute('alt', weatherData.weather[0].description);
 
-    temperature.innerHTML = `${weatherData.main.temp}&deg;F`;
+    temperature.innerHTML = `${Math.round(weatherData.main.temp)}&deg;F`;
     weatherDesc.innerHTML = weatherData.weather[0].description;
-    high.innerHTML = `High: ${weatherData.main.temp_max}&deg;F`;
-    low.innerHTML = `Low: ${weatherData.main.temp_min}&deg;F`;
+    high.innerHTML = `High: ${Math.round(weatherData.main.temp_max)}&deg;F`;
+    low.innerHTML = `Low: ${Math.round(weatherData.main.temp_min)}&deg;F`;
     humidity.innerHTML = `Humidity: ${weatherData.main.humidity}%`;
 
     // After trying these two lines and having them not work I researched more about how to convert the Unix timestamp to a date object and then get the hours and minutes from that date object.
@@ -87,11 +85,11 @@ function displayCurrentWeather(weatherData) {
 
     // Something still didn't seem right with the sunrise and sunset times. Then I realized that the times were in UTC and I needed to convert them to the local time zone of Europe/Berlin.
     const sunriseDate = new Date(weatherData.sys.sunrise * 1000);
-    const sunRiseFormatted = new Intl.DateTimeFormat('en-US', {timeStyle: 'short', timeZone: 'Europe/Berlin'}).format(sunriseDate);
+    const sunRiseFormatted = new Intl.DateTimeFormat('en-US', { timeStyle: 'short', timeZone: 'Europe/Berlin' }).format(sunriseDate);
     sunRise.innerHTML = `Sunrise: ${sunRiseFormatted}`;
 
     const sunsetDate = new Date(weatherData.sys.sunset * 1000);
-    const sunSetFormatted = new Intl.DateTimeFormat('en-US', {timeStyle: 'short', timeZone: 'Europe/Berlin'}).format(sunsetDate);
+    const sunSetFormatted = new Intl.DateTimeFormat('en-US', { timeStyle: 'short', timeZone: 'Europe/Berlin' }).format(sunsetDate);
     sunSet.innerHTML = `Sunset: ${sunSetFormatted}`;
 
     weatherCard.appendChild(icon);
@@ -109,61 +107,63 @@ function displayCurrentWeather(weatherData) {
 
 // Function to display forecast weather data
 function displayForecastWeather(forecastData) {
+    const spanNumber0 = document.querySelector('.day0');
+    const spanNumber1 = document.querySelector('.day1');
+    const spanNumber2 = document.querySelector('.day2');
 
-    const forecastContainer = document.createElement('div');
-    forecastContainer.setAttribute('class', 'forecast-container');
-
-    forecastData.list.forEach((forecast) => {
-        const forecastCard = document.createElement('div');
-        forecastCard.setAttribute('class', 'forecast-card');
-
-        const date = document.createElement('p');
-        date.setAttribute('class', 'forecast-date');
-        date.innerHTML = new Date(forecast.dt * 1000).toLocaleDateString();
-
-        const temperature = document.createElement('p');
-        temperature.setAttribute('class', 'forecast-temperature');
-        temperature.innerHTML = `${forecast.main.temp}&deg;F`;
-
-        const weatherDesc = document.createElement('p');
-        weatherDesc.setAttribute('class', 'forecast-description');
-        weatherDesc.innerHTML = forecast.weather[0].description;
-
-        forecastCard.appendChild(date);
-        forecastCard.appendChild(temperature);
-        forecastCard.appendChild(weatherDesc);
-        forecastContainer.appendChild(forecastCard);
-    });
-
-    document.querySelector('#forecast').appendChild(forecastContainer);
+    spanNumber0.innerHTML = `${Math.round(forecastData.list[0].main.temp)}&deg;F`;
+    spanNumber1.innerHTML = `${Math.round(forecastData.list[1].main.temp)}&deg;F`;
+    spanNumber2.innerHTML = `${Math.round(forecastData.list[2].main.temp)}&deg;F`;
 }
 
-getCurrentWeather();
-getForecastWeather();
-
-
+// THis is my function for creating "Today" and the names of the next two days: Depending on the current day of the week
 function getWeatherDays() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const today = new Date();
     const weatherDays = [];
 
     for (let i = 0; i < 3; i++) {
+        // The % 7 is used to wrap around the daysOfWeek array
         const dayIndex = (today.getDay() + i) % 7;
-        weatherDays.push(daysOfWeek[dayIndex]);
+        // Set today as Today then the next two days as the next days of the week
+        // Basiclly if i is 0 then today, if i is 1 then tomorrow, if i is 2 then the day after tomorrow
+        // those are the 3 days I'm displaying in the forecast
+        let dayToday;
+        if (i == 0) {
+            dayToday = "Today";
+        } else {
+            dayToday = daysOfWeek[dayIndex];
+        }
+        // push will add the next day to the end of the array
+        weatherDays.push(dayToday);
     }
 
-    // Display in the DOM
-    const dayList = document.getElementById('day-list');
-    dayList.innerHTML = ''; // Clear previous content
+    weatherDays.forEach((day, i) => {
+        const forecastDiv = document.createElement('div');
+        const forecastDays = document.createElement('p');
+        const forecastSpan = document.createElement('span');
 
-    weatherDays.forEach(day => {
-        const li = document.createElement('li');
-        li.textContent = day;
-        dayList.appendChild(li);
+        forecastDiv.setAttribute('class', day);
+        forecastDays.setAttribute('class', 'day');
+        // Just a little trick to add the index of the array to the class of the span
+        forecastSpan.setAttribute('class', `day${i}`);
+
+        forecastDays.innerHTML = `${day}: `;
+
+        forecastDays.appendChild(forecastSpan);
+        forecastDiv.appendChild(forecastDays);
+
+        forecastTemp.appendChild(forecastDiv);
+
+        //console.log(`${i}: ${day}`);
     });
 
     //console.log(today);
     //console.log(weatherDays);
+    
+    
 }
 
 getWeatherDays();
+getCurrentWeather();
+getForecastWeather();
